35cc7baf5283305a54b7169a7907108b7fb70332
adsonlee <adsonlee01@gmail.com>
2025-12-10 20:50:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "index.html", // 确保以 index.html 为入口
-    },
-  },
+  base: '/miner-cat/',  // 🔥 必须加
 });
