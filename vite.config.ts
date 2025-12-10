d634db593e3321b620b7c83fa6a0c08e95a29203
adsonlee <adsonlee01@gmail.com>
2025-12-10 22:00:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/miner-cat/',
   plugins: [react()],
-});
+  // 添加这一行，确保使用相对路径加载资源
+  base: './', 
+})
