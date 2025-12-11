fe578e85fcb142f444be270c3bbf6b803d6689e8
adsonlee <adsonlee01@gmail.com>
2025-12-11 09:20:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 把 './' 改为你的 '/仓库名/' (注意前后都有斜杠)
+  // 必须有这一行，且必须是你的仓库名，前后都要有斜杠
   base: '/miner-cat/', 
+  build: {
+    outDir: 'dist',
+  }
 })
