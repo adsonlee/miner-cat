d9cbaef7bbb44bc930637aad8e32fe8cf53920ff
adsonlee <adsonlee01@gmail.com>
2025-12-11 08:34:40 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // 核心修复点：确保这里包含了 src 下的所有 ts/tsx 文件
   content: [
     "./index.html",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./services/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
